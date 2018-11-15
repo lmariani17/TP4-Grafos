e370ead5be6ae6facba99a7dd047adeeafa9c59e
Ignacio Arias <johnmermanson@gmail.com>
2018-11-15 15:04:50 -0300

    Cambios del 15/11

---
+++ src/grafos/GrafoAleatorioPorPorcDeAdyacencia.java
@@ -0,0 +1,17 @@
+package grafos;
+
+public class GrafoAleatorioPorPorcDeAdyacencia extends Generador {
+
+	public GrafoAleatorioPorPorcDeAdyacencia(int nodos) {
+		super(nodos);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public void generar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
